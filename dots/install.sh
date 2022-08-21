d14915d2acaa2e593f6512cb968ec410878238f3
sioel0 <leonisimone@protonmail.com>
2022-08-21 12:43:48 +0200

    Added tpm plugin manager and fixed dots installation script

--- dots/install.sh
+++ dots/install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-rm ~/.bashrc
 # setup home directory files
 stow --dir=dots/ --target=/home/$USER home
 
-mkdir ~/.config
 # setup .config directory
 stow --dir=dots --target=/home/$USER/.config config

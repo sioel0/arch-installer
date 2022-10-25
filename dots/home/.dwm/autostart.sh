f4923ead07ce2d815c0b1eb96e30735f7ffc4631
sioel0 <leonisimone@protonmail.com>
2022-10-25 16:20:01 +0200

    Moved dotfiles to dotfiles repo and changed install.yml

--- dots/home/.dwm/autostart.sh
+++
@@ -1,6 +0,0 @@
-~/.scripts/screen_setup
-nitrogen --restore
-setxkbmap us -variant altgr-intl
-xmodmap ~/.Xmodmap
-dunst &
-telegram-desktop -startintray
--- dots/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm ~/.bashrc ~/.zshrc
-# setup home directory files
-stow --dir=dots/ --target=/home/$USER home
-
-# setup .config directory
-stow --dir=dots --target=/home/$USER/.config config
